706f0af4bf1deea64138812a1bcbd186805ecb9a
Akash Panwar <akashpanwar9976@gmail.com>
2022-10-25 19:18:43 +0530

    Outputting a list of meetups

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const dummy_meetups = [
+  {
+    id: 'm1',
+    title: 'A first meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some city',
+    description: 'This is the first meetup',
+  },
+  {
+    id: 'm2',
+    title: 'A second meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some city',
+    description: 'This is the second meetup',
+  },
+  //   {
+  //     id: 'm3',
+  //     name: 'A third meetup',
+  //     image:
+  //       'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+  //     address: 'Some address 5, 12345 Some city',
+  //     description: 'This is the third meetup',
+  //   },
+];
+
+export default function HomePage() {
+  return <MeetupList meetups={dummy_meetups} />;
+}

083ff95ce6b1ef7bb30d7ece08ca1abb96e5cfe0
Akash Panwar <akashpanwar9976@gmail.com>
2022-10-26 20:15:21 +0530

    custom components & CSS modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      address='Some Street 5, Some City'
+      title='A First Meetup'
+      description='The meetup description'
+      image='https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg'
+    />
+  );
+}
+
+export default MeetupDetails;
